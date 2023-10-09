3572c8dd5d688e652bec2e9e7753ee18f995daaf
Diana Lenceviciene <dilencevi@gmail.com>
2023-10-09 15:00:15 -0400

    Restructured the script

--- src/NTimeLine.py
+++ src/NTimeLine.py
@@ -2,24 +2,18 @@ import sqlite3
 import pandas as pd
 import sys
 
-if len(sys.argv) != 3:
-    print ("""\
-            This script connects to the SQLite database, performs the query to get the timeline 
-            of events and returns a pandas DataFrame containing the results. The results are
-            printed out into a file.
-
-            Usage: NTimeLine <path_to_the_SQLite_file> <path_to_and_name_of_output_file>
-            """)
-    sys.exit(0)
-
-# Get a path to a SQLite file
-db_path = sys.argv[1]
-
-# Get a path and file name for an output
-output_file = sys.argv[2]
-
 # Function to perform query and return DataFrame
 def get_event_timeline(sqlite_file):
+    '''
+    Connects to the SQLite database, performs a query to retrieve the timeline of events with timestamp, module string, event string, and stackDepth.
+    Returns DataFrame object.
+
+        Parameters:
+            sqlite_file: A file created from Nvidia Nsight Systems profile
+        Returns:
+            DataFrame object
+    '''
+
     # Connect to the SQLite database
     conn = sqlite3.connect(sqlite_file)
 
@@ -42,16 +36,37 @@ def get_event_timeline(sqlite_file):
     # Execute the query and store the result in a DataFrame
     df = pd.read_sql(query, conn)
 
-    # Write the DataFrame to a CSV file
-    df.to_csv(output_file, index=False)
-
     # Close the connection to the database
     conn.close()
 
     return df
 
-# Get the timeline DataFrame
-timeline_df = get_event_timeline(db_path)
+def main():
+    if len(sys.argv) != 3:
+        print ("""\
+            This script connects to the SQLite database, performs the query to get the timeline
+            of events and returns a pandas DataFrame containing the results. The results are
+            printed out into a file.
 
-# Print the first few rows of the DataFrame
-print(timeline_df.head())
+            Usage: NTimeLine <path_to_the_SQLite_file> <path_to_and_name_of_output_file>
+            """)
+        sys.exit(0)
+
+    # Get a path to a SQLite file
+    db_path = sys.argv[1]
+
+    # Get a path and file name for an output
+    output_file = sys.argv[2]
+
+    # Get the timeline DataFrame
+    timeline_df = get_event_timeline(db_path)
+
+    # Write the DataFrame to a CSV file
+    timeline_df.to_csv(output_file, index=False)
+
+    # Print the first few rows of the DataFrame
+    print(timeline_df.head())
+
+if __name__ == "__main__":
+    main()
+
